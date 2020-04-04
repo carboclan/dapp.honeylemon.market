34762929f477e4a91d5530efffacabb02a6c57fb
Christopher Maree <christopher.maree@gmail.com>
2020-04-04 14:42:40 +0200

    refactor bridge

--- contracts/honeylemon/MarketContractProxy.sol
+++ contracts/honeylemon/MarketContractProxy.sol
@@ -8,20 +8,43 @@ import "../marketprotocol/mpx/MarketContractFactoryMPX.sol";
 
 contract MarketContractProxy is Ownable {
 
-          // Settles old contract and deploys the new contract
+    MarketContractFactoryMPX marketContractFactoryMPX;
+    
+    address public HONEY_LEMON_ORACLE_ADDRESS;
+    address public 0X_MINTER_BRIDGE_ADDRESS;
 
-    function settle(uint currentIndexValue, uint loobackIndexValue) public onlyHoneyLemonOracle {
+    constructor(address marketContractFactoryMPX, address honeyLemonOracle, address 0xMinterBridge){
 
+        marketContractFactoryMPX = MarketContractFactoryMPX(marketContractFactoryMPX);
+        HONEY_LEMON_ORACLE_ADDRESS = honeyLemonOracle;
+        0X_MINTER_BRIDGE_ADDRESS = 0xMinterBridge;
     }
 
-    // It is much safer to deploy Market contracts from the contract. This ensures that the code deployed 
-    // cannot be substituted (e.g. to steal the collateral)
-    function deployContract(uint indexValue) onlyOwner (address) public internal {
-        // Deploys the current day Market contract. `indexValue` is used to initialize collateral requirement in its constructor
-        // Stores the new contract address, block it was deployed in, as well as the value of the index
-        // we’ll need easy access to the latest values of contract address and index.
-        // collateral requirement = indexValue * 28 * overcollateralization_factor
-        // returns the address of the new contract
+    modifier onlyHoneyLemonOracle(){
+        require(msg.sender == HONEY_LEMON_ORACLE_ADDRESS);
+        _;
+    }
+
+    modifier onlyMinterBridge(){
+        require(msg.sender == 0X_MINTER_BRIDGE_ADDRESS);
+        _;
+    }
+
+    marketProtocolContractSpecifications[7];
+
+
+    function balanceOf(address _owner) public view returns (uint256 balance) public {
+        // Return `balanceOf` for current day PositionTokenLong
+    }
+
+    // What’s the TH amount that can currently be filled based on owner’s BTC balance and allowance
+    function fillableAmount(address _owner) public view returns (uint256 amount) {
+        // return min(imBTC.balanceOf(_owner), imBTC.allowance(_owner, MARKET_PROTOCOL_POOL_ADDRESS)) / (indexValue * CONTRACT_DURATION)
+    }
+
+              // Settles old contract and deploys the new contract
+    function settle(uint currentIndexValue, uint lookbackIndexValue, uint timestamp) public onlyHoneyLemonOracle {
+
     }
 
     function mintPositionTokens(
@@ -34,14 +57,21 @@ contract MarketContractProxy is Ownable {
         // CURRENT_CONTRACT_ADDRESS -> COLLATERAL_POOL_ADDRESS
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) public {
-        // Return `balanceOf` for current day PositionTokenLong
+     // It is much safer to deploy Market contracts from the contract. This ensures that the code deployed 
+    // cannot be substituted (e.g. to steal the collateral)
+    function deployContract(uint indexValue) onlyOwner (address) internal {
+        // Deploys the current day Market contract. `indexValue` is used to initialize collateral requirement in its constructor
+        // Stores the new contract address, block it was deployed in, as well as the value of the index
+        // we’ll need easy access to the latest values of contract address and index.
+        // collateral requirement = indexValue * 28 * overcollateralization_factor
+        // returns the address of the new contract
     }
 
-    // What’s the TH amount that can currently be filled based on owner’s BTC balance and allowance
-    function fillableAmount(address _owner) public view returns (uint256 amount) {
-        // return min(imBTC.balanceOf(_owner), imBTC.allowance(_owner, MARKET_PROTOCOL_POOL_ADDRESS)) / (indexValue * CONTRACT_DURATION)
-    }
+    function pushOraclePriceIndex(uint currentIndexValue, uint lookbackIndexValue, uint timestamp) internal {}
+
+    function generateContractSpecs() internal returns(unit[7]){},
+
+    function generateContractNames() internal returns(bytes32[3]){},
 
 
 }
