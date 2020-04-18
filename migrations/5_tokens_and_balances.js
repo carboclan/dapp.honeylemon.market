a8e74593ca57d39f4275dc2c99a1e1d2709b7b54
Christopher Maree <christopher.maree@gmail.com>
2020-04-18 19:30:09 +0200

    updated balances

--- migrations/5_tokens_and_balances.js
+++ migrations/5_tokens_and_balances.js
@@ -3,11 +3,11 @@ const CollateralToken = artifacts.require('CollateralToken');
 
 module.exports = async function(deployer, network, accounts) {
   // Deploy USDC token
-  await deployer.deploy(PaymentToken, 'USDC', 'USDC', '10000000', 6);
+  await deployer.deploy(PaymentToken, 'USDC', 'USDC', '100000000', 6);
   const paymentToken = await PaymentToken.deployed();
 
   // Give some payment token to investor
-  await paymentToken.transfer(accounts[2], '10000000');
+  await paymentToken.transfer(accounts[2], '100000000');
 
   console.log(' 💰Transferred balances');
 };
