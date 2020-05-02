05ce3c5636b827371a8fcfe000d2fe100f4a56b1
Michael Yankelev <michael@linumlabs.com>
2020-05-02 17:15:27 +0200

    fix more styles

--- webapp/src/components/BuyContractPage.tsx
+++ webapp/src/components/BuyContractPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Typography, Grid, makeStyles, FilledInput, Link } from '@material-ui/core';
 
-const useStyles = makeStyles(({spacing}) => ({
+const useStyles = makeStyles(({ spacing }) => ({
   rightAlign: {
     textAlign: 'end',
   },
@@ -28,19 +28,25 @@ const BuyContractPage: React.SFC = () => {
       <Grid item xs={6} className={classes.rightAlign}><Typography color='secondary'>$0.115 Th/day</Typography></Grid>
       <Grid item xs={12}><Typography style={{ fontWeight: 'bold' }}>ENTER BUDGET</Typography></Grid>
       <Grid item xs={10} className={classes.rightAlign}>
-        <FilledInput fullWidth disableUnderline inputProps={{className: classes.inputBase}}/>
+        <FilledInput
+          fullWidth
+          disableUnderline
+          inputProps={{ className: classes.inputBase }}
+          onChange={e => setTotalPrice(Number.parseFloat(e.target.value))} />
       </Grid>
       <Grid item xs={2} className={classes.rightAlign}>
         <Typography style={{ fontWeight: 'bold' }} color='secondary'>USDT</Typography>
       </Grid>
       <Grid item xs={12}><Typography className={classes.rightAlign}>31.06 Th for 28 days</Typography></Grid>
-      <Button fullWidth>BUY NOW</Button>
-      <Typography>
-        You will pay ${totalPrice} buy {hashAmount} Th of hasrate for 28 days for ${hashPrice} Th/day.
-        You will receive the average value of the <Link href='#'>Mining Revenue Index</Link> over 28 days.
-        Representing {totalHashAmount} Th of mining power per day per contract.
-      </Typography>
-      <Link href='#'>See full contract specification here.</Link>
+      <Button fullWidth>BUY NOW</Button><Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <Typography>
+          You will pay ${totalPrice} buy {hashAmount} Th of hasrate for 28 days for ${hashPrice} Th/day.
+          You will receive the average value of the <Link href='#'>Mining Revenue Index</Link> over 28 days.
+          Representing {totalHashAmount} Th of mining power per day per contract.
+        </Typography>
+      </Grid>
+      <Grid item><Typography>See <Link href='#'>full contract specification here.</Link></Typography></Grid>
     </Grid>
   )
 }
