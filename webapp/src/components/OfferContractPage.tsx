a367c4b4aec41a54f97e4cf8db3944e2f7726841
Michael Yankelev <michael@linumlabs.com>
2020-05-02 18:14:35 +0200

    wire up offer page

--- webapp/src/components/OfferContractPage.tsx
+++ webapp/src/components/OfferContractPage.tsx
@@ -1,40 +1,72 @@
-import React from 'react';
-import { Button, Typography, Grid, makeStyles } from '@material-ui/core';
-import { InputNumber } from 'antd';
+import React, { useState } from 'react';
+import { Button, Typography, Grid, makeStyles, FilledInput, Link, InputAdornment } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(({ spacing }) => ({
   rightAlign: {
     textAlign: 'end',
+  },
+  inputBase: {
+    textAlign: 'end',
+    padding: spacing(1)
   }
 }))
 
 const OfferContractPage: React.SFC = () => {
   // const { wallet, onboard, address, network, balance, notify } = useOnboard();
+  const [hashPrice, setHashPrice] = useState(0);
+  const [hashAmount, setHashAmount] = useState(0);
+  const [totalHashPrice, setTotalHashPrice] = useState(0);
+  const [btcAmount, setBtcAmount] = useState(0);
+
   const classes = useStyles();
   return (
-    <Grid container alignItems='stretch' justify='center'>
+    <Grid container alignItems='flex-start' justify='flex-start' spacing={2}>
       <Grid item xs={12}>
-        <Typography variant='h6'>Buy Mining Rewards</Typography>
+        <Typography style={{ fontWeight: 'bold' }}>Offer a 28 day Mining Revenue Contract</Typography>
       </Grid>
-      <Grid item xs={6}><Typography>PRICE</Typography></Grid>
-      <Grid item xs={6} className={classes.rightAlign}><Typography>$0.115 Th/day</Typography></Grid>
-      <Grid item xs={6}><Typography>ENTER BUDGET</Typography></Grid>
-      <Grid item xs={6} className={classes.rightAlign}>
-        <InputNumber
-          min={0}
-          onChange={value => console.log(value)}
-          size='small'
-        />
+      <Grid item xs={6}><Typography style={{ fontWeight: 'bold' }}>Price:</Typography></Grid>
+      <Grid item xs={4}>
+        <FilledInput
+          fullWidth
+          disableUnderline
+          inputProps={{ className: classes.inputBase }}
+          placeholder='100'
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          onChange={e => setHashPrice(Number.parseFloat(e.target.value))} 
+          value={hashPrice} />
       </Grid>
-      <Typography variant='h6'>31.06 Th for 28 days</Typography>
-      <hr />
-      <Button color="primary">BUY NOW</Button>
-      <Typography variant='h6'>
-        You will pay $100 buy 31.05 Th of hasrate for 28 days for $0.115 Th/day.
-        You will receive the average value of the Mining Revenue Index over 28 days.
-        Representing 883.2 Th of mining power per day per contract.
-      </Typography>
-      <Typography variant='h6'>See full contract specification here.</Typography>
+      <Grid item xs={2} className={classes.rightAlign}>
+        <Typography style={{ fontWeight: 'bold' }} color='secondary'>Th/day</Typography>
+      </Grid>
+      <Grid item xs={6}><Typography style={{ fontWeight: 'bold' }}>Quantity</Typography></Grid>
+      <Grid item xs={4}>
+        <FilledInput
+          fullWidth
+          disableUnderline
+          inputProps={{ className: classes.inputBase }}
+          placeholder='100'
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          onChange={e => setHashAmount(Number.parseFloat(e.target.value))}
+          value={hashAmount} />
+      </Grid>
+      <Grid item xs={2} className={classes.rightAlign}>
+        <Typography style={{ fontWeight: 'bold' }} color='secondary'>Th</Typography>
+      </Grid>
+      <Grid item xs={6}><Typography style={{ fontWeight: 'bold' }}>Total:</Typography></Grid>
+      <Grid item xs={4} style={{textAlign: 'center'}}><Typography style={{ fontWeight: 'bold' }}>${totalHashPrice}</Typography></Grid>
+      <Grid item xs={12}><Typography style={{fontStyle: 'italic', fontSize: 12}}>${hashPrice} Th/day * 28 Days * {hashAmount} Contracts</Typography></Grid>
+      <Grid item xs={12}><Button fullWidth>BUY NOW</Button></Grid>
+      <Grid item xs={12}>
+        <Typography>
+          You will offer {hashAmount} contracts at ${hashPrice} Th/day. 
+          If a hodler buys your offer you will receive ${setTotalHashPrice} USDT. 
+          You will be asked to post the hodlers max win of {btcAmount} BTC as collateral. 
+          The amount of that collateral that the hodler receives will be determined 
+          by the average value of the Mining Revenue Index over the 28 days starting 
+          when the hodler pays you.
+        </Typography>
+      </Grid>
+      <Grid item><Typography>See <Link href='#'>full contract specification here.</Link></Typography></Grid>
     </Grid>
   )
 }
