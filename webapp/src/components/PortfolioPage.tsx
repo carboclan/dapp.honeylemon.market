e922bd790bd2a6eaa1115921f024ad68b60a46ba
Michael Yankelev <myankelev@gmail.com>
2020-05-30 04:36:36 +0200

    wire up portfolio page

--- webapp/src/components/PortfolioPage.tsx
+++ webapp/src/components/PortfolioPage.tsx
@@ -13,13 +13,14 @@ import {
   TableCell,
   Table,
   TableBody,
-  Divider
+  Divider,
+  CircularProgress
 } from '@material-ui/core';
 import { RadioButtonUnchecked } from '@material-ui/icons';
 import { useOnboard } from '../contexts/OnboardContext';
 import { useHoneylemon } from '../contexts/HoneylemonContext';
 
-const useStyles = makeStyles(({ spacing }) => ({
+const useStyles = makeStyles(({ spacing, palette }) => ({
   icon: {
     marginLeft: spacing(1),
   },
@@ -32,15 +33,28 @@ const useStyles = makeStyles(({ spacing }) => ({
   sectionDivider: {
     margin: spacing(2),
     height: spacing(0.5),
+  },
+  loadingSpinner: {
+    width: 20,
+    flexBasis: 'end',
+    flexGrow: 0,
+    color: palette.secondary.main,
+  },
+  sectionHeading: {
+    fontWeight: 'bold',
+    color: palette.secondary.main,
+  },
+  placeholderRow: {
+    height: 60,
   }
 }))
 
 const PorfolioPage: React.SFC = () => {
   const { address } = useOnboard();
-  const { honeylemonService, CONTRACT_DURATION } = useHoneylemon();
+  const { honeylemonService, CONTRACT_DURATION, COLLATERAL_TOKEN_DECIMALS } = useHoneylemon();
 
   const [activeTab, setActiveTab] = useState<'active' | 'settled'>('active')
-  const [collateralForWithdraw, setCollateralForWithdraw] = useState(0);
+  const [collateralForWithdraw, setCollateralForWithdraw] = useState<Number>(0);
   const [openOrdersMetadata, setOpenOrdersMetadata] = useState<
     Array<{
       orderHash: string,
@@ -71,6 +85,8 @@ const PorfolioPage: React.SFC = () => {
   const [settledPositionsToWithdraw, setSettledPositionsToWithdraw] = useState([]);
   const [settledPositions, setSettledPositions] = useState([]);
   const [refresh, setRefresh] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isWithdrawing, setIsWithdrawing] = useState(false);
 
   const handleSetActiveTab = (event: React.ChangeEvent<{}>, newValue: 'active' | 'settled') => {
     setActiveTab(newValue);
@@ -91,11 +107,23 @@ const PorfolioPage: React.SFC = () => {
       .then(() => setRefresh(true));
   }
 
+  const withdrawAvailable = async () => {
+    setIsWithdrawing(true);
+    try {
+      const results = await honeylemonService.batchRedeem(address);
+      setRefresh(true);
+    } catch (error) {
+      console.log("Something went wrong during the withdrawal");
+      console.log(error);
+    }
+    setIsWithdrawing(false);
+  }
+
   useEffect(() => {
     let cancelled = false;
 
     const getPorfolio = async () => {
-
+      setIsLoading(true);
       const openOrdersRes = await honeylemonService.getOpenOrders(address);
       const positions = await honeylemonService.getPositions(address);
       if (!cancelled) {
@@ -104,28 +132,42 @@ const PorfolioPage: React.SFC = () => {
           openOrdersRes.records.map(((openOrder: any) => [openOrder.metaData.orderHash, openOrder.order]))
         ));
 
-        const allPositions = positions.longPositions.map((lc: any) => ({
-          ...lc,
-          contractName: lc.contractName + '-long',
-          daysToMaturity: Math.ceil(dayjs(lc.time * 1000).add(CONTRACT_DURATION, 'd').diff(dayjs(), 'd', true))
-        })).concat(positions.shortPositions.map((sc: any) => ({
-          ...sc,
-          contractName: sc.contractName + '-short',
-          daysToMaturity: Math.ceil(dayjs(sc.time * 1000).add(CONTRACT_DURATION, 'd').diff(dayjs(), 'd', true))
-        })));
+        const allPositions = positions.longPositions.map((lp: any) => ({
+          ...lp,
+          contractName: lp.contractName + '-long',
+        })).concat(positions.shortPositions.map((sp: any) => ({
+          ...sp,
+          contractName: sp.contractName + '-short',
+        }))).map((p: any) => ({
+          ...p,
+          daysToMaturity: Math.ceil(dayjs(p.contract.expiration * 1000).diff(dayjs(), 'd', true)),
+          pendingReward: Number(p.pendingReward?.shiftedBy(-COLLATERAL_TOKEN_DECIMALS).toString()) || 0,
+          finalReward: Number(p.finalReward?.shiftedBy(-COLLATERAL_TOKEN_DECIMALS).toString()) || 0,
+        }));
 
-        setActivePositions(allPositions.filter((p: any) => !p.contract.settlement));
-        const sctw = allPositions.filter((c: any) => c.daysToMaturity <= 0 && c?.pendingReward?.gt(0))
+        const activePositions = allPositions.filter((p: any) => !p?.contract.settlement)
+        setActivePositions(activePositions);
+        const sctw = allPositions.filter((p: any) => !!p?.contract.settlement /** &&  outstanding withdraw flag */)
         setSettledPositionsToWithdraw(sctw);
-        setCollateralForWithdraw(sctw.reduce((total: BigNumber, contract: any) => total.plus(contract?.pendingReward), new BigNumber(0)));
-        setSettledPositions(allPositions.filter((c: any) => c.daysToMaturity <= 0 && c?.pendingReward.eq(0)))
+        setCollateralForWithdraw(sctw.reduce((total: Number, contract: any) => total += contract?.finalReward, 0));
+        const finalized = allPositions.filter((p: any) => !!p?.contract.settlement /** &&  !outstanding withdraw flag */)
+        setSettledPositions(finalized);
         setRefresh(false);
+        setIsLoading(false)
       }
     }
-    getPorfolio();
+    getPorfolio()
     return () => { cancelled = true }
-
   }, [address, honeylemonService, refresh, CONTRACT_DURATION])
+
+  useEffect(() => {
+    var poller: NodeJS.Timeout;
+    poller = setInterval(() => setRefresh(true), 5000);
+
+    return () => {
+      clearInterval(poller)
+    }
+  }, [])
 
   const classes = useStyles();
   return (
@@ -146,7 +188,9 @@ const PorfolioPage: React.SFC = () => {
         <div className={classes.tabContent}>
           {activeTab === 'active' ?
             <>
-              <Button fullWidth>NEW OFFER</Button>
+              <Typography variant='h5' className={classes.sectionHeading}>
+                Unfilled Positions
+              </Typography>
               <Table>
                 <TableHead>
                   <TableRow>
@@ -163,30 +207,47 @@ const PorfolioPage: React.SFC = () => {
                       <TableCell align='right'><Button onClick={() => cancelOpenOrder(order.orderHash)}>Cancel</Button></TableCell>
                     </TableRow>
                   )}
+                  <TableRow>
+                    <TableCell colSpan={3} align='center' className={classes.placeholderRow}>
+                      {isLoading && <CircularProgress className={classes.loadingSpinner} size={20} />}
+                      {!isLoading && openOrdersMetadata.length === 0 && "No open positions"}
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
               <Divider className={classes.sectionDivider} light variant='middle' />
-              <Typography variant='h5' style={{ fontWeight: 'bold' }} color='secondary'>Positions</Typography>
+              <Typography variant='h5' className={classes.sectionHeading}>
+                Positions
+                </Typography>
               <Table>
                 <TableHead>
                   <TableRow>
                     <TableCell>Swap</TableCell>
                     <TableCell>Quantity</TableCell>
                     <TableCell>Days</TableCell>
+                    <TableCell align='right'>BTC Accrued</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {activePositions && activePositions?.map((contract: any, i) =>
+                  {activePositions && activePositions?.map((position: any, i) =>
                     <TableRow key={i}>
-                      <TableCell>{contract.contractName}</TableCell>
-                      <TableCell align='center'>{contract.qtyToMint}</TableCell>
-                      <TableCell>{contract.daysToMaturity}</TableCell>
+                      <TableCell>{position.contractName}</TableCell>
+                      <TableCell align='center'>{position?.qtyToMint}</TableCell>
+                      <TableCell>{position.daysToMaturity}</TableCell>
+                      <TableCell>{position.pendingReward}</TableCell>
                     </TableRow>
                   )}
+                  <TableRow>
+                    <TableCell colSpan={4} align='center' className={classes.placeholderRow}>
+                      {isLoading && <CircularProgress className={classes.loadingSpinner} size={20} />}
+                      {!isLoading && activePositions.length === 0 && "No active positions"}
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </> :
             <>
+              <Typography variant='h5' className={classes.sectionHeading}>Withdraw Pending</Typography>
               <Table>
                 <TableHead>
                   <TableRow>
@@ -196,22 +257,31 @@ const PorfolioPage: React.SFC = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {settledPositionsToWithdraw?.map((contract: any, i) =>
+                  {settledPositionsToWithdraw?.map((position: any, i) =>
                     <TableRow key={i}>
-                      <TableCell>{contract.contractName}</TableCell>
-                      <TableCell align='center'>{contract.qtyToMint}</TableCell>
-                      <TableCell align='right'>{contract.qtyToMint}</TableCell>
+                      <TableCell>{position.contractName}</TableCell>
+                      <TableCell align='center'>{position.qtyToMint}</TableCell>
+                      <TableCell align='right'>{position.finalReward}</TableCell>
                     </TableRow>
                   )}
+                  <TableRow>
+                    <TableCell colSpan={3} align='center' className={classes.placeholderRow}>
+                      {isLoading && <CircularProgress className={classes.loadingSpinner} size={20} />}
+                      {!isLoading && settledPositionsToWithdraw.length === 0 && "No open positions"}
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
-              <Button fullWidth disabled={collateralForWithdraw === 0}>
-                WITHDRAW ALL {collateralForWithdraw > 0 ?
-                  `(${collateralForWithdraw} BTC)` :
-                  <RadioButtonUnchecked className={classes.icon} />}
+              <Button fullWidth disabled={collateralForWithdraw === 0} onClick={withdrawAvailable}>
+                {(!isWithdrawing) ?
+                  (collateralForWithdraw > 0) ?
+                    `WITHDRAW ALL (${collateralForWithdraw} BTC)` :
+                    <>WITHDRAW ALL <RadioButtonUnchecked className={classes.icon} /></> :
+                  <>WITHDRAW ALL <CircularProgress className={classes.loadingSpinner} size={20} /></>
+                }
               </Button>
               <Divider className={classes.sectionDivider} light variant='middle' />
-              <Typography variant='h5' style={{ fontWeight: 'bold' }} color='secondary'>Closed</Typography>
+              <Typography variant='h5' className={classes.sectionHeading}>Closed</Typography>
               <Table>
                 <TableHead>
                   <TableRow>
@@ -221,13 +291,19 @@ const PorfolioPage: React.SFC = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {settledPositions.map((contract: any, i) =>
+                  {settledPositions.map((position: any, i) =>
                     <TableRow key={i}>
-                      <TableCell>{contract.contractName}</TableCell>
-                      <TableCell align='center'>{contract.qtyToMint}</TableCell>
-                      <TableCell align='right'>{contract.qtyToMint}</TableCell>
+                      <TableCell>{position.contractName}</TableCell>
+                      <TableCell align='center'>{position.qtyToMint}</TableCell>
+                      <TableCell align='right'>{position.finalReward}</TableCell>
                     </TableRow>
                   )}
+                  <TableRow>
+                    <TableCell colSpan={3} align='center' className={classes.placeholderRow}>
+                      {isLoading && <CircularProgress className={classes.loadingSpinner} size={20} />}
+                      {!isLoading && settledPositions.length === 0 && "No open positions"}
+                    </TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </>
