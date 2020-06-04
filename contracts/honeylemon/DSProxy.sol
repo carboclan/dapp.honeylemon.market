120cd06c5c2a703a6cb580409dbdfb6e993534df
Christopher Maree <christopher.maree@gmail.com>
2020-06-04 22:31:50 +0200

    updated removal of DSProxy

--- contracts/honeylemon/DSProxy.sol
+++ contracts/honeylemon/DSProxy.sol
@@ -96,24 +96,7 @@ contract DSAuth is DSAuthEvents {
 // the proxy can be changed, this allows for dynamic ownership models
 // i.e. a multisig
 contract DSProxy is DSAuth, DSNote {
-    DSProxyCache public cache; // global cache for contracts
-
     function() external payable {}
-
-    // use the proxy to execute calldata _data on contract _code
-    // function execute(bytes memory _code, bytes memory _data)
-    //     public
-    //     payable
-    //     returns (address target, bytes memory response)
-    // {
-    //     target = cache.read(_code);
-    //     if (target == address(0)) {
-    //         // deploy contract & store its address in cache
-    //         target = cache.write(_code);
-    //     }
-
-    //     response = execute(target, _data);
-    // }
 
     function execute(address _target, bytes memory _data)
         public
@@ -157,12 +140,10 @@ contract DSProxy is DSAuth, DSNote {
 contract DSProxyFactory {
     event Created(address indexed sender, address indexed owner, address proxy);
     mapping(address => bool) public isProxy;
-    DSProxyCache public cache;
     address marketContractProxy;
 
     constructor() public {
         marketContractProxy = msg.sender;
-        // cache = new DSProxyCache();
     }
 
     modifier onlyMarketContractProxy() {
@@ -190,37 +171,5 @@ contract DSProxyFactory {
     // sets owner of proxy to caller
     function build() internal returns (address payable proxy) {
         proxy = build(msg.sender);
-    }
-}
-
-
-// DSProxyCache
-// This global cache stores addresses of contracts previously deployed
-// by a proxy. This saves gas from repeat deployment of the same
-// contracts and eliminates blockchain bloat.
-
-// By default, all proxies deployed from the same factory store
-// contracts in the same cache. The cache a proxy instance uses can be
-// changed.  The cache uses the sha3 hash of a contract's bytecode to
-// lookup the address
-contract DSProxyCache {
-    mapping(bytes32 => address) cache;
-
-    function read(bytes memory _code) public view returns (address) {
-        bytes32 hash = keccak256(_code);
-        return cache[hash];
-    }
-
-    function write(bytes memory _code) public returns (address target) {
-        assembly {
-            target := create(0, add(_code, 0x20), mload(_code))
-            switch iszero(extcodesize(target))
-                case 1 {
-                    // throw if contract failed to deploy
-                    revert(0, 0)
-                }
-        }
-        bytes32 hash = keccak256(_code);
-        cache[hash] = target;
     }
 }
