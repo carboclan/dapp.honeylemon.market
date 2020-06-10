6e27d156fefb50abbe3d6f5ec98fc288851474d0
Andrei Anisimov <1471231+andrei-anisimov@users.noreply.github.com>
2020-06-09 21:13:54 -0700

    converted market protocol migration to await

--- migrations/2_market_protocol.js
+++ migrations/2_market_protocol.js
@@ -8,47 +8,25 @@ const MarketContractRegistry = artifacts.require('./MarketContractRegistry.sol')
 module.exports = async function(deployer, network, accounts) {
   if (network == 'skip-migrations') return;
 
-  // TODO: refactor this to not use .then notation in favour of using awaits
   // Note ownership transfer of MarketContractFactory, MarketCollateralPool and MarketContractRegistry occur
   // in a later migration after the MarketContractProxy has been deployed.
-  deployer.deploy(StringLib).then(function() {
-    return deployer.deploy(MathLib).then(function() {
-      return deployer.deploy(MarketContractRegistry).then(function() {
-        return deployer
-          .link(MathLib, [MarketContractMPX, MarketCollateralPool, MarketContractFactory])
-          .then(function() {
-            return deployer.link(StringLib, MarketContractMPX).then(function() {
-              return deployer
-                .deploy(
-                  MarketCollateralPool,
-                  MarketContractRegistry.address,
-                  '0x0000000000000000000000000000000000000000' // Market token Address should be unset.
-                )
-                .then(function() {
-                  return MarketCollateralPool.deployed().then(function() {
-                    return deployer
-                      .deploy(
-                        MarketContractFactory,
-                        MarketContractRegistry.address,
-                        MarketCollateralPool.address,
-                        accounts[0]
-                      )
-                      .then(function(factory) {
-                        return MarketContractRegistry.deployed().then(function(
-                          registryInstance
-                        ) {
-                          return registryInstance
-                            .addFactoryAddress(factory.address)
-                            .then(function() {
-                              console.log('💹 Done Market Migration!');
-                            });
-                        });
-                      });
-                  });
-                });
-            });
-          });
-      });
-    });
-  });
+  await deployer.deploy(StringLib);
+  await deployer.deploy(MathLib);
+  await deployer.deploy(MarketContractRegistry);
+  await deployer.link(MathLib, [MarketContractMPX, MarketCollateralPool, MarketContractFactory]);
+  await deployer.link(StringLib, MarketContractMPX);
+  await deployer.deploy(
+    MarketCollateralPool,
+    MarketContractRegistry.address,
+    '0x0000000000000000000000000000000000000000' // Market token Address should be unset.
+  );
+  const factory = await deployer.deploy(
+    MarketContractFactory,
+    MarketContractRegistry.address,
+    MarketCollateralPool.address,
+    accounts[0]
+  );
+  const registryInstance = await MarketContractRegistry.deployed();
+  await registryInstance.addFactoryAddress(factory.address);
+  console.log('💹 Done Market Migration!');
 };
