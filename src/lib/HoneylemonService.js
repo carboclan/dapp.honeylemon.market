b33e49d5d2ea917575a65751497a0d0117792d5b
Michael Yankelev <myankelev@gmail.com>
2020-06-21 15:03:19 +0200

    revert kovan deployment settings

--- src/lib/HoneylemonService.js
+++ src/lib/HoneylemonService.js
@@ -25,8 +25,8 @@ const TH_DECIMALS = 0; // TH has 6 decimals
 const PAYMENT_TOKEN_DECIMALS = 6; // USDT has 6 decimals
 const COLLATERAL_TOKEN_DECIMALS = 8; // imBTC has 8 decimals
 const SHIFT_PRICE_BY = TH_DECIMALS - PAYMENT_TOKEN_DECIMALS;
-// const CONTRACT_DURATION = 28; // Days
-const CONTRACT_DURATION = 2; // 2 day for Kovan deployment
+const CONTRACT_DURATION = 28; // Days
+// const CONTRACT_DURATION = 2; // 2 day for Kovan deployment
 
 class HoneylemonService {
   constructor(
