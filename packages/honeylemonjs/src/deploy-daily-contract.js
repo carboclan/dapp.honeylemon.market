8d9aa46981a548fb7d1ea0dbfa832e1503d9ad17
Michael Yankelev <myankelev@gmail.com>
2020-06-22 22:10:14 +0200

    update js package to use TS, remove webpack

--- packages/honeylemonjs/src/deploy-daily-contract.js
+++ packages/honeylemonjs/src/deploy-daily-contract.js
@@ -39,7 +39,7 @@ const MarketContractMPX = artifacts.require('marketContractMPX');
 const MarketCollateralPool = artifacts.require('MarketCollateralPool');
 
 // Calculation constants
-const necessaryCollateralRatio = 0.35; // for 135% collateralization
+const necessaryCollateralRatio = 0.25; // for 125% collateralization
 const collateralDecimals = 1e8; // scaling for imBTC (8 decimal points)
 const paymentDecimals = 1e6; // scaling for USDT or USDC (6 decimals)
 
