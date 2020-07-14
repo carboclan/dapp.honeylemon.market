e4b987c6f31d39f4abb4e95d07c73519c3e6898e
Michael Yankelev <myankelev@gmail.com>
2020-07-14 23:14:27 +0200

    add config for wbtc

--- packages/webapp/src/contexts/HoneylemonConfig.ts
+++ packages/webapp/src/contexts/HoneylemonConfig.ts
@@ -7,7 +7,7 @@ type ConfigSet = {
   minterBridgeAddress: string;
 }
 
-export interface HoneylemonConfig {
+interface HoneylemonConfig {
   [networkId: number]: ConfigSet
 }
 
@@ -15,6 +15,7 @@ const config: HoneylemonConfig = {
   // The first key is the default network that the app expected. This will also be the network that
   // Onboard.js instructs the user to switch to if they select an invalid network
   1: {
+    // This is the imBTC Mainnet instance
     apiUrl: "https://api.honeylemon.market/sra/v3",
     subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon",
     collateralTokenAddress: "0x3212b29e33587a00fb1c83346f5dbfa69a458923",
@@ -22,6 +23,15 @@ const config: HoneylemonConfig = {
     marketContractProxy: "0xDCe36F47ea4b3b367E424baC1D37Bb62E1361342",
     minterBridgeAddress: "0xF5F91F83872727aB02E8AfED2ca8e14EF2cA34C0",
   },
+  // 1: {
+  //   // This is the WBTC Mainnet instance
+  //   apiUrl: "https://api.honeylemon.market/sra/v3",
+  //   subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon",
+  //   collateralTokenAddress: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  //   paymentTokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  //   marketContractProxy: "0x5B3fab261Fd55C79552297f1A0Cc97B35dC8631a",
+  //   minterBridgeAddress: "0xF905F6f2a482e39199BDC60b9257A62618e08C9a",
+  // },
   42: {
     apiUrl: "https://api.hldev.net:3000/sra/v3/",
     subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon-kovan",
@@ -33,3 +43,8 @@ const config: HoneylemonConfig = {
 }
 
 export default config;
+
+//
+//
+//
+//
