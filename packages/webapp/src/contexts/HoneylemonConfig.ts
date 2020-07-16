6676458aafc0e7db53b840884318a737a46420e8
Michael Yankelev <myankelev@gmail.com>
2020-07-17 01:14:39 +0200

    update config

--- packages/webapp/src/contexts/HoneylemonConfig.ts
+++ packages/webapp/src/contexts/HoneylemonConfig.ts
@@ -15,31 +15,22 @@ const config: HoneylemonConfig = {
   // The first key is the default network that the app expected. This will also be the network that
   // Onboard.js instructs the user to switch to if they select an invalid network
   1: {
-    // This is the imBTC Mainnet instance
+    // This is the WBTC Mainnet instance
     apiUrl: "https://api.honeylemon.market/sra/v3",
     subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon",
-    collateralTokenAddress: "0x3212b29e33587a00fb1c83346f5dbfa69a458923",
+    collateralTokenAddress: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
     paymentTokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    marketContractProxy: "0xDCe36F47ea4b3b367E424baC1D37Bb62E1361342",
-    minterBridgeAddress: "0xF5F91F83872727aB02E8AfED2ca8e14EF2cA34C0",
+    marketContractProxy: "0x5B3fab261Fd55C79552297f1A0Cc97B35dC8631a",
+    minterBridgeAddress: "0xF905F6f2a482e39199BDC60b9257A62618e08C9a",
   },
-  // 1: {
-  //   // This is the WBTC Mainnet instance
-  //   apiUrl: "https://api.honeylemon.market/sra/v3",
-  //   subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon",
-  //   collateralTokenAddress: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-  //   paymentTokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-  //   marketContractProxy: "0x5B3fab261Fd55C79552297f1A0Cc97B35dC8631a",
-  //   minterBridgeAddress: "0xF905F6f2a482e39199BDC60b9257A62618e08C9a",
-  // },
-  42: {
-    apiUrl: "https://api.hldev.net:3000/sra/v3/",
-    subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon-kovan",
-    collateralTokenAddress: "0xE2F58b9747e0b417C0D4c36390Ea40E1e064D592",
-    paymentTokenAddress: "0x3CE983761C26c2F36CB438a3B0103Aa72D43B299",
-    marketContractProxy: "0x3270070240747337Dbc27C7C7CD70D1921098eD2",
-    minterBridgeAddress: "0xE874aD2963dc4BE26A69E6bA0112023aAD0Cd1c1",
-  }
+  // 42: {
+  //   apiUrl: "https://api.hldev.net:3000/sra/v3/",
+  //   subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon-kovan",
+  //   collateralTokenAddress: "0xE2F58b9747e0b417C0D4c36390Ea40E1e064D592",
+  //   paymentTokenAddress: "0x3CE983761C26c2F36CB438a3B0103Aa72D43B299",
+  //   marketContractProxy: "0x3270070240747337Dbc27C7C7CD70D1921098eD2",
+  //   minterBridgeAddress: "0xE874aD2963dc4BE26A69E6bA0112023aAD0Cd1c1",
+  // }
 }
 
 export default config;
