4b6d72dd77cff56c3369b0334d63d0b05b783e7c
Michael Yankelev <myankelev@gmail.com>
2020-07-24 09:59:26 +0200

    update prod config

--- packages/webapp/src/contexts/HoneylemonConfig.ts
+++ packages/webapp/src/contexts/HoneylemonConfig.ts
@@ -14,6 +14,15 @@ interface HoneylemonConfig {
 const config: HoneylemonConfig = {
   // The first key is the default network that the app expected. This will also be the network that
   // Onboard.js instructs the user to switch to if they select an invalid network
+  1: {
+    // This is the WBTC Mainnet instance
+    apiUrl: "https://api.honeylemon.market/sra/v3",
+    subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon",
+    collateralTokenAddress: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    paymentTokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    marketContractProxy: "0x9ceb5486eD0F3F2DBCaE906E4192472e88657983",
+    minterBridgeAddress: "0x5D35B6dB6d6FF772A2F9B660D028aa671752b644",
+  },
   42: {
     apiUrl: "https://api.hldev.net:3000/sra/v3/",
     subgraphUrl: "https://api.thegraph.com/subgraphs/name/carboclan/honeylemon-kovan",
